9dd6bf12fe38ba5e166ecc312fd6e16fc63e7739
vincent <hkvincentchan@gmail.com>
2023-11-19 21:06:22 +0800

    remove the swr

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -1,5 +1,7 @@
 import useSWR from "swr";
 import Post from "./Post";
+import { useEffect, useState } from "react";
+import LoadingSVG from "./LoadingSVG";
 
 function PostList({
     index,
@@ -12,18 +14,36 @@ function PostList({
     showEditBtn?: boolean;
     numberOfPosts?: number;
 }) {
-    console.log("PostList" +  numberOfPosts );
-    const { data, error, isLoading } = useSWR(
-        () => "/api/posts?page=" + index + "&username=" + username
-    );
+    console.log("PostList" + numberOfPosts);
+    const [post, setPost] = useState<PostI[]>([])
+    const [isLoading, setIsLoading] = useState(true)
 
 
-    if (error) return <div>failed to load</div>;
-    if (isLoading || !data) return <div>loading...</div>;
+    // async useEffect to fetch data from api and set it to post state.
+
+
+    useEffect(() => {
+        async function fetchMyAPI() {
+            const response = await fetch("/api/posts?page=" + index + "&username=" + username)
+            const data = await response.json()
+            console.log({ data })
+            setIsLoading(false);
+            setPost(data.data)
+        }
+        fetchMyAPI()
+    }, [index, numberOfPosts, username])
+
+    // const { data, error, isLoading } = useSWR(
+    //     () => "/api/posts?page=" + index + "&username=" + username
+    // );
+
+
+    // if (error) return <div>failed to load</div>;
+    if (isLoading || !post) return <div className="flex flex-row justify-center items-center"><LoadingSVG /></div>;
 
     return (
         <ul>
-            {data.data.map((post: PostI) => {
+            {post.map((post: PostI) => {
                 return (
                     <li className="my-5" key={post.id}>
                         <Post post={post} showEditBtn={showEditBtn} />
